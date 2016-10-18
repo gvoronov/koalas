ef202d542af6e1dbcf3015aa6b9af58fa9b72a99
gvoronov@penlink.com <GVoronov@GVoronovLT.penlink.com>
2016-10-18 11:59:23 -0600

    row and data obj starting to work

--- src/main/scala/DataElement.scala
+++ src/main/scala/DataElement.scala
@@ -1,32 +1,52 @@
 // Option 1
-abstract class DataElement
-case class Numerical(val element: Double) extends DataElement {
+import math._
+
+abstract class DataValue
+
+case class NumericalValue(val element: Double) extends DataElement {
   def apply(): Double = element
+  def +(val x: Double): NumericalValue = NumericalValue(element + x)
+  def +(val x: NumericalValue): NumericalValue = NumericalValue(element + x())
+
+  def -(val x: Double): NumericalValue = NumericalValue(element - x)
+  def -(val x: NumericalValue): NumericalValue = NumericalValue(element - x())
+
+  def *(val x: Double): NumericalValue = NumericalValue(element * x)
+  def *(val x: NumericalValue): NumericalValue = NumericalValue(element * x())
+
+  def /(val x: Double): NumericalValue = NumericalValue(element / x)
+  def /(val x: NumericalValue): NumericalValue = NumericalValue(element / x())
+
+  def **(val x: Double): NumericalValue = NumericalValue(pow(element, x))
+  def **(val x: NumericalValue): NumericalValue = NumericalValue(pow(element, x()))
 }
-case class Categorical(val element: String) extends DataElement {
+
+// Investigate using a companion object to maintain list of possible categories
+case class CategoricalValue(val element: String) extends DataElement {
   def apply(): String = element
 }
+// case class DateValue()
 
 // Option 2
-abstract sealed trait DataElement {
-  val numerical: Double
-  val categorical: String
-
-  def isNumerical(): Boolean = ! numerical.equals(Double.NaN)
-  def isCategorical(): Boolean = !(categorical == null)
-}
-case class Numerical(val numerical: Double) extends DataElement {
-  val categorical: String = null
-  def apply(): Double = numerical
-}
-case class Categorical(val categorical: String) extends DataElement {
-  val numerical: Double = Double.NaN
-  def apply(): String = categorical
-}
+// abstract sealed trait DataElement {
+//   val numerical: Double
+//   val categorical: String
+//
+//   def isNumerical(): Boolean = ! numerical.equals(Double.NaN)
+//   def isCategorical(): Boolean = !(categorical == null)
+// }
+// case class Numerical(val numerical: Double) extends DataElement {
+//   val categorical: String = null
+//   def apply(): Double = numerical
+// }
+// case class Categorical(val categorical: String) extends DataElement {
+//   val numerical: Double = Double.NaN
+//   def apply(): String = categorical
+// }
 
 // Option 3
-class DataElement[T](val element: T) {
-  def apply(): T = element
-}
-class Numerical(element: Double) extends DataElement[Double](element)
-class Categorical(element: String) extends DataElement[String](element)
+// class DataElement[T](val element: T) {
+//   def apply(): T = element
+// }
+// class Numerical(element: Double) extends DataElement[Double](element)
+// class Categorical(element: String) extends DataElement[String](element)
--- src/main/scala/Row.scala
+++ src/main/scala/Row.scala
@@ -1,23 +1,36 @@
-class Row(params: List[Tuple3[String, Byte, Any]]){
-  val groupedParams = params.groupBy(_._2)
-  val numericalMap: Map[String, Double] =  groupedParams(110)
-    .asInstanceOf[List[Tuple3[String, Byte, Double]]]
-    .map(x => (x._1 -> x._3)).toMap
-  val categoricalMap: Map[String, String] = groupedParams(99)
-    .asInstanceOf[List[Tuple3[String, Byte, String]]]
-    .map(x => (x._1 -> x._3)).toMap
-  val typeMap: Map[String, Byte] = params.map(x => (x._1, x._2)).toMap
-
-  def select(column: String): Either[String, Double] = {
-    typeMap(column) match {
-      case 'c' => Left(categoricalMap(column))
-      case 'n' => Right(numericalMap(column))
-    }
-  }
+// class Row(params: List[Tuple3[String, Byte, Any]]){
+//   val groupedParams = params.groupBy(_._2)
+//   val numericalMap: Map[String, Double] =  groupedParams(110)
+//     .asInstanceOf[List[Tuple3[String, Byte, Double]]]
+//     .map(x => (x._1 -> x._3)).toMap
+//   val categoricalMap: Map[String, String] = groupedParams(99)
+//     .asInstanceOf[List[Tuple3[String, Byte, String]]]
+//     .map(x => (x._1 -> x._3)).toMap
+//   val typeMap: Map[String, Byte] = params.map(x => (x._1, x._2)).toMap
+//
+//   def select(column: String): Either[String, Double] = {
+//     typeMap(column) match {
+//       case 'c' => Left(categoricalMap(column))
+//       case 'n' => Right(numericalMap(column))
+//     }
+//   }
 
   // val numericalMap: Map[String, Double] = Map()
   // val categoricalMap: Map[String, String] = Map()
   //
   //
   // val numericMap: Map[String, Double] = Map()
+// }
+
+case class Row(val paramMap: Map[String, DataValuev]){
+  def apply[T](column: String): T = paramMap(column).asInstanceOf[T]
+
+  def select(column: String): DataValue = paramMap(column)
+  def select(columns: String*): Row = Row(paramMap filterKeys columns.toSet)
+  def select(columns: Set[String]): Row = Row(paramMap filterKeys columns)
+  def select(columns: Iterable[String]): Row = Row(paramMap filterKeys columns.toSet)
+
+  def +(kvPair: Tuple2[String, DataValue]): Row = Row(paramMap + kvPair)
+
+  def ++(otherRow: Row): Row = Row(paramMap ++ otherRow.paramMap)
 }
