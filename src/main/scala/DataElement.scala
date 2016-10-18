ae195adf3494311c06c546847bd86eafa9cb3aa1
Gennady Voronov <gvoronov@gmail.com>
2016-10-18 07:11:24 -0600

    play w/ dataelement

--- src/main/scala/DataElement.scala
+++ src/main/scala/DataElement.scala
@@ -1,4 +1,5 @@
-sealed trait DataElement
+// Option 1
+abstract class DataElement
 case class Numerical(val element: Double) extends DataElement {
   def apply(): Double = element
 }
@@ -6,7 +7,26 @@ case class Categorical(val element: String) extends DataElement {
   def apply(): String = element
 }
 
+// Option 2
+abstract sealed trait DataElement {
+  val numerical: Double
+  val categorical: String
 
-// sealed trait DataElement[Double, String]
-// case class Numerical[Double, String](a: Double) extends DataElement[Double, String]
-// case class Categorical[Double, String](b: String) extends DataElement[Double, String]
+  def isNumerical(): Boolean = ! numerical.equals(Double.NaN)
+  def isCategorical(): Boolean = !(categorical == null)
+}
+case class Numerical(val numerical: Double) extends DataElement {
+  val categorical: String = null
+  def apply(): Double = numerical
+}
+case class Categorical(val categorical: String) extends DataElement {
+  val numerical: Double = Double.NaN
+  def apply(): String = categorical
+}
+
+// Option 3
+class DataElement[T](val element: T) {
+  def apply(): T = element
+}
+class Numerical(element: Double) extends DataElement[Double](element)
+class Categorical(element: String) extends DataElement[String](element)
