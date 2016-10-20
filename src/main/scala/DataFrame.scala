e1e97779ad6e81d40a8d199db369213edea6baad
Gennady Voronov <gvoronov@gmail.com>
2016-10-19 22:42:53 -0600

    dataframe started

--- src/main/scala/DataFrame.scala
+++ src/main/scala/DataFrame.scala
@@ -1,10 +1,30 @@
-import scala.collection.immutable.LinearSeq
+class DataFrame(val rowVector: Vector[Row]) extends Series[Row](rowVector) {
+
+}
+
+object DataFrame{
+  def apply(rowVector: Vector[Row]): DataFrame = new DataFrame(rowVector)
+  def apply(columnMap: Map[String, Vector[DataValue]]) = {
+    val length: Int = columnMap(columnMap.keysIterator.next).length
+    def buildRow(i: Int): Row = Row(columnMap.map(column => column._1 -> column._2(i)))
+    val rowVector: Vector[Row] = Range(0, length).map(buildRow).toVector
+
+    new DataFrame(rowVector)
+  }
+  // def apply(columnMap: Map[String, Series[DataValue]]) = {
+  //   new DataFrame(rowIterable)
+  // }
+}
+
+
+
+// import scala.collection.immutable.LinearSeq
 
 // class DataFrame extends LinearSeq[Row]
-class DataFrame(val rows: LinearSeq[Row]) extends LinearSeq[Row] {
-  def apply(idx: Int): Row = rows(idx)
-  def length: Int = rows.length
-}
+// class DataFrame(val rows: LinearSeq[Row]) extends LinearSeq[Row] {
+//   def apply(idx: Int): Row = rows(idx)
+//   def length: Int = rows.length
+// }
 //
 // // object DataFrame {
 // //   def apply() = new DataFrame
--- src/main/scala/Series.scala
+++ src/main/scala/Series.scala
@@ -1,1 +1,12 @@
-class Series{}
+class Series[T](val values: Vector[T]){
+  def apply(index: Int): T = values(index)
+
+  def map[R](f: (T) => R): Vector[R] = values.map(f)
+  def reduce[R >: T](op: (R, R) => R): R = values.reduce(op)
+  def filter(p: (T) => Boolean): Vector[T] = values.filter(p)
+  def groupBy[R](f: (T) => R): Map[R, Vector[T]] = values.groupBy(f)
+}
+
+object Series{
+  def apply[T](values: Vector[T]): Series[T] = new Series[T](values)
+}
