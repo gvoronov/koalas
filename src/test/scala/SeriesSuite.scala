65ac48e8cd756800c18b0bfb984c9d37d102c805
gvoronov@penlink.com <GVoronov@GVoronovLT.penlink.com>
2016-11-03 10:31:45 -0600

    add series test

---
+++ src/test/scala/SeriesSuite.scala
@@ -0,0 +1,14 @@
+package koalas.test
+
+import org.scalatest.FunSuite
+import org.scalatest.Assertions._
+
+// import koalas.datasets.bikesharing.BikeSharing
+import koalas.datavalue._
+import koalas.series.Series
+
+class SeriesSuite extends FunSuite {
+  test("get numerical value series and sum it") {
+
+  }
+}
