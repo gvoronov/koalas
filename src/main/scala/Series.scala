2d6dfdad2125cfe48b8d39eeabcfcf8c4d70439d
Gennady Voronov <gvoronov@gmail.com>
2016-11-03 23:58:37 -0600

    int commit

--- src/main/scala/Series.scala
+++ src/main/scala/Series.scala
@@ -18,25 +18,59 @@ class Series[+T](val values: Vector[T]){
     (Series[T](left), Series[T](right))
   }
 
+  lazy val last: T = values.last
+
   private def binaryOpOnAny[D, R](that: Any, op: (D, D) => R): Series[R] = {
     that match {
-      case series: Series[Any] => series(0) match {
-        case element: NumericalValue => Series(
-          values.zip(series.values).map(pair => op(
-            pair._1.asInstanceOf[D], pair._2.asInstanceOf[D]))
-        )
+      // case series: Series[Any] => series(0) match {
+      //   case element: NumericalValue => {
+      //     assert(last.isInstanceOf[NumericalValue])
+      //     Series(values.zip(series.values).map(pair => op(pair._1.asInstanceOf[D], pair._2.asInstanceOf[D])))
+      //   }
+      //   case _ => throw new RuntimeException("")
+      // }
+      case series: Series[Any] => {
+        series(0) match {
+          case _: NumericalValue => assert(last.isInstanceOf[NumericalValue])
+          case _: Double => assert(last.isInstanceOf[Double])
+          case _: Int => assert(last.isInstanceOf[Int])
+        }
+        Series(values.zip(series.values).map(pair => op(pair._1.asInstanceOf[D], pair._2.asInstanceOf[D])))
+      }
+      case iterable: Iterable[Any] => {
+        iterable.last match {
+          case _: NumericalValue => assert(last.isInstanceOf[NumericalValue])
+          case _: Double => assert(last.isInstanceOf[Double])
+          case _: Int => assert(last.isInstanceOf[Int])
+        }
+        Series(values.zip(iterable).map(pair => op(pair._1.asInstanceOf[D], pair._2.asInstanceOf[D])).toVector)
+      }
+
+
+      // case value: NumericalValue => last match {
+      //   case _: NumericalValue => Series(values.map(y => op(y.asInstanceOf[D], value.asInstanceOf[D])))
+      //   case _: Double => Series(values.map(y => op(y.asInstanceOf[D], value().asInstanceOf[D])))
+      //   case _: Int => Series(values.map(y => op(y.asInstanceOf[D], value().asInstanceOf[D])))
+      //   case _ => throw new RuntimeException("")
+      // }
+      // case value: Double => last match {
+      //   case _: NumericalValue => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(value).asInstanceOf[D])))
+      //   case _: Double | _: Int => Series(values.map(y => op(y.asInstanceOf[D], value.asInstanceOf[D])))
+      //   case _: Int => Series(values.map(y => op(y.asInstanceOf[D], value.asInstanceOf[D])))
+      //   case _ => throw new RuntimeException("")
+      // }
+      case value: NumericalValue => last match {
+        case _: NumericalValue => Series(values.map(y => op(y.asInstanceOf[D], value.asInstanceOf[D])))
+        case _: Double | _: Int => Series(values.map(y => op(y.asInstanceOf[D], value().asInstanceOf[D])))
         case _ => throw new RuntimeException("")
       }
-      case iterable: Iterable[Any] => iterable.last match {
-        case element: NumericalValue => Series(
-          values.zip(iterable).map(pair => op(
-            pair._1.asInstanceOf[D], pair._2.asInstanceOf[D])).toVector
-        )
+      case _: Double | _: Int => last match {
+        case _: NumericalValue => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(that.asInstanceOf[Double]).asInstanceOf[D])))
+        case _: Double | _: Int => Series(values.map(y => op(y.asInstanceOf[D], that.asInstanceOf[D])))
         case _ => throw new RuntimeException("")
       }
-      case value: NumericalValue => Series(values.map(y => op(y.asInstanceOf[D], value.asInstanceOf[D])))
-      case value: Double => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(value).asInstanceOf[D])))
-      case value: Int => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(value).asInstanceOf[D])))
+      // case value: Double => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(value).asInstanceOf[D])))
+      // case value: Int => Series(values.map(y => op(y.asInstanceOf[D], NumericalValue(value).asInstanceOf[D])))
       case _ => throw new RuntimeException(
         "Series binary opertion attempted with non-numerical type")
     }
@@ -47,10 +81,61 @@ class Series[+T](val values: Vector[T]){
    * pattern match onto the the appropriate domaon type of the binary op.
    */
   // private def selectBinaryOpInType[A](that: Any):
-  def +(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a + b)
-  def -(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a - b)
-  def *(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a * b)
-  def /(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a / b)
+  // def +(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a + b)
+
+  trait NumericLike {
+    def +(that: DataValue): DataValue
+    def +(that: NumericalValue): NumericalValue = this + that
+  }
+  def +[A <: DataValue with NumericLike](that: Any): Series[T] = binaryOpOnAny[A, A](that, (a: A, b: A) => (a + b).asInstanceOf[A]).asInstanceOf[Series[T]]
+
+  // def +(that: Any): Series[T] = {
+  //   last match {
+  //     case _: NumericalValue =>
+  //       binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a + b).asInstanceOf[Series[T]]
+  //     case _: Double =>
+  //       binaryOpOnAny[Double, Double](that, (a, b) => a + b).asInstanceOf[Series[T]]
+  //     case _: Int =>
+  //       binaryOpOnAny[Int, Int](that, (a, b) => a + b).asInstanceOf[Series[T]]
+  //     case _ => throw new RuntimeException("")
+  //   }
+  // }
+  def -(that: Any): Series[T] = {
+    last match {
+      case _: NumericalValue =>
+        binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a - b).asInstanceOf[Series[T]]
+      case _: Double =>
+        binaryOpOnAny[Double, Double](that, (a, b) => a - b).asInstanceOf[Series[T]]
+      case _: Int =>
+        binaryOpOnAny[Int, Int](that, (a, b) => a - b).asInstanceOf[Series[T]]
+      case _ => throw new RuntimeException("")
+    }
+  }
+  def *(that: Any): Series[T] = {
+    last match {
+      case _: NumericalValue =>
+        binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a * b).asInstanceOf[Series[T]]
+      case _: Double =>
+        binaryOpOnAny[Double, Double](that, (a, b) => a * b).asInstanceOf[Series[T]]
+      case _: Int =>
+        binaryOpOnAny[Int, Int](that, (a, b) => a * b).asInstanceOf[Series[T]]
+      case _ => throw new RuntimeException("")
+    }
+  }
+  def /(that: Any): Series[T] = {
+    last match {
+      case _: NumericalValue =>
+        binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a / b).asInstanceOf[Series[T]]
+      case _: Double =>
+        binaryOpOnAny[Double, Double](that, (a, b) => a / b).asInstanceOf[Series[T]]
+      case _: Int =>
+        binaryOpOnAny[Int, Int](that, (a, b) => a / b).asInstanceOf[Series[T]]
+      case _ => throw new RuntimeException("")
+    }
+  }
+  // def -(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a - b)
+  // def *(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a * b)
+  // def /(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a / b)
   def **(that: Any): Series[NumericalValue] = binaryOpOnAny[NumericalValue, NumericalValue](that, (a, b) => a ** b)
 
   def :>(that: Any): Series[Boolean] = binaryOpOnAny[NumericalValue, Boolean](that, (a, b) => a > b)
